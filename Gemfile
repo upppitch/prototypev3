f42da69b6bbbc303e5a6d57a639f5fbe1cc13c2a
Lemonjai <evilleodidi@gmail.com>
2016-05-17 21:53:53 -0400

    fix the styling for the job post

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'devise'
 # Image upload
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'simple_form'
+gem 'bootstrap-sass', '~> 3.3.6'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   resources :jobs
 
   root 'jobs#index'
+
+  devise_scope :user do
+   get "signup", to: "devise/registrations#new"
+   get "login", to: "devise/sessions#new"
+   get "logout", to: "devise/sessions#destroy"
+  end
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
